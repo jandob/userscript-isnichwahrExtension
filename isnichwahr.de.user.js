23901fce7128d7fedd1e5fb78c1ecd928b131b5a
jandob <janosch.dobler@gmail.com>
2017-11-19 19:03:27 +0100

    update to 2.0

--- isnichwahr.de.user.js
+++ isnichwahr.de.user.js
@@ -1,221 +1,288 @@
 // ==UserScript==
-// @name         isnichwahrExtension
+// @name         isnichwahrExtensionES7
 // @namespace    http://jandob.com
-// @version      1.1
+// @version      2.0
 // @description  extends isnichwahr functionality
 // @author       jandob
 // @match        http://www.isnichwahr.de/*
 // @grant        GM_openInTab
 // @grant        GM_info
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @require      https://unpkg.com/vue
+// @run-at       document-end
 // @updateURL    https://github.com/jandob/userscripts/raw/master/isnichwahr.de.user.js
 // ==/UserScript==
-var isTampermonkey = GM_info.scriptHandler === "Tampermonkey";
-var $ = jQuery;
 
-$('.service-links').remove();
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+/* jshint esnext: false */
+/* jshint esversion: 6 */
+/* jshint asi: true */
 
-if (window.location.href.indexOf("isnichwahr.de/r") > -1) {
-    $('.navbar').remove();
-    $('body').attr('style', 'padding-top: 10px !important');
-    $('div.panel-panel.top').remove();
+const isTampermonkey = GM_info.scriptHandler === 'Tampermonkey'
+const $ = jQuery
 
-    $('div.panel-panel.right').remove();
-    $('div.panel-panel.left').removeClass (function (index, css) {
-        return (css.match(/(^|\s)col-\S+/g) || []).join(' ');
-    });
-    $('div.panel-panel.left').addClass('col-xs-12 col-md-10 col-md-offset-1');
+class Storage {
+    async init(storageUri) {
+        this.baseUrl = 'https://api.myjson.com/bins'
+        if (storageUri) {
+          this.storageUri = storageUri
+        } else {
+          this.storageUri = localStorage.getItem('synchronized_storage_uri')
+        }
+        if (!this.storageUri) {
+           console.log('init new storage')
+           this.storageUri = await fetch(this.baseUrl, {
+             method: "POST",
+             body: JSON.stringify({}),
+             headers: new Headers({
+                'Content-Type':"application/json; charset=utf-8",
+                'Accept': 'application/json'
+             })
+           }).then(response => response.json())
+             .then(data => data.uri)
+        }
+        // we have a storage uri now
+        localStorage.setItem('synchronized_storage_uri', this.storageUri)
+
+        this.data = await fetch(this.storageUri)
+          .then(response => response.json())
+        this.synced = true
+        console.log("finished init",this.storageUri)
+    }
+    _push_request(val) {
+      return fetch(this.storageUri, {
+         method: "PUT",
+         body: JSON.stringify(val),
+         headers: new Headers({
+            'Content-Type':"application/json; charset=utf-8",
+            'Accept': 'application/json'
+         })
+      })
+    }
+    // pushes to storage, makes sure the latest push is executed
+    _push (val) {
+	  if (this.currentRequest) {
+        this.pendingRequest = val
+        return
+      }
+      this.currentRequest = true
+      this._push_request(val).then( () => {
+         console.log("push done")
+         this.currentRequest = false
+         if (this.pendingRequest) {
+           this._push(this.pendingRequest)
+           this.pendingRequest = false
+         }
+      })
+    }
+    set(val) {
+      localStorage.setItem('synchronized_storage', JSON.stringify(val))
+      this._push(val)
+    }
+    get() {
+      return JSON.parse(localStorage.getItem('synchronized_storage'))
+    }
 }
-if (window.location.href.indexOf("isnichwahr.de/list") > -1) {
+$('.service-links').remove()
+// matches individual links
+if (window.location.href.indexOf('isnichwahr.de/r') > -1) {
+    $('.navbar').remove()
+    $('body').attr('style', 'padding-top: 10px !important')
+    $('div.panel-panel.top').remove()
 
-    var storageUri = localStorage.getItem("visitedLinksUUID");
-    var visitedLinks = JSON.parse(localStorage.getItem("visitedLinks"));
-    if (visitedLinks === undefined || visitedLinks === null) {
-        visitedLinks = {};
-        localStorage.setItem("visitedLinks", JSON.stringify(visitedLinks));
-    }
-
-    var newLinks = [];
-    var $currentLinks = $('.view-link-liste tbody tr td:nth-child(3) a');
-    var currentLinks = {};
-    function updateVisited() {
-        newLinks = [];
-        $currentLinks.each( function(i, element) {
-            if (!($(this).attr('href') in visitedLinks)) {
-                $(this).css( "color", "red" );
-                newLinks.push(this);
-            } else {
-                $(this).removeAttr('style');
-            }
-        });
-    }
-    var storage = {
-        create: function() {
-            $.ajax({
-                url:"https://api.myjson.com/bins",
-                type:"POST",
-                data: JSON.stringify(visitedLinks),
-                contentType:"application/json; charset=utf-8",
-                dataType:"json"
-            }).done( function(data) {
-                storageUri=data.uri;
-                localStorage.setItem("visitedLinksUUID", data.uri);
-                window.location.reload();
-            });
-        },
-        set: function () {
-            var data=JSON.stringify(visitedLinks);
-            localStorage.setItem("visitedLinks", data);
-            $.ajax({
-                url: storageUri,
-                type:"PUT",
-                data: data,
-                contentType:"application/json; charset=utf-8",
-                dataType:"json"
-            }).done( function(data) {
-                visitedLinks = data;
-                localStorage.setItem("visitedLinks", JSON.stringify(visitedLinks));
-            });
-        },
-        get: function () {
-            visitedLinks=JSON.parse(localStorage.getItem("visitedLinks"));
-            updateVisited();
-            return $.get(storageUri).done( function(data) {
-                visitedLinks=data;
-                localStorage.setItem("visitedLinks", JSON.stringify(visitedLinks));
-                updateVisited();
-                console.log('got visitedLinks from server, nr of links: ',
-                            $.map(visitedLinks, function(n, i) { return i; }).length);
-            });
-        }
-    };
-    if (storageUri === null || storageUri === undefined || storageUri == "undefined" ) {
-        console.log("creating new storageUri");
-        storage.create(visitedLinks);
-    } else {
-        storage.get();
-
-        if (visitedLinks.length > 1000) {
-            $.each(visitedLinks, function(link, type) {
-                if (!(currentLinks.hasOwnProperty(link))) {
-                    delete visitedLinks[link];
-                }
-            });
-            storage.set();
-        }
-
-        $currentLinks.each(function() {
-            var url = $(this).attr('href');
-            currentLinks[url] = getType(this);
-        });
-        // initialize newLinks
-        $currentLinks.each( function(i, element) {
-            $(this).click( function (event) {
-                event.preventDefault();
-                openLink(this, false);
-            });
-        });
-
-        function getType(element) {
+    $('div.panel-panel.right').remove()
+    $('div.panel-panel.left').removeClass (function (index, css) {
+        return (css.match(/(^|\s)col-\S+/g) || []).join(' ')
+    })
+    $('div.panel-panel.left').addClass('col-xs-12 col-md-10 col-md-offset-1')
+}
+function getType(element) {
             var elementTypes = {
-                '.fa-film, .fa-youtube': 'video',
-                '.glyphicon-picture': 'pics'
-            };
-            var foundType = false;
+                '.fa-youtube': 'video',
+                '.fa-film': 'gif',
+                '.glyphicon-picture': 'pic'
+            }
+            var foundType = false
             $.each(elementTypes, function(selector, type) {
                 if ($(element).parent().parent().has(selector)[0]) {
-                    foundType = type;
-                    return false; //break
+                    foundType = type
+                    return false //break
                 }
-            });
-            return foundType;
+            })
+            return foundType
         }
-
-        function openLink(element, open_in_background) {
-            var url = $(element).attr('href');
-            $(element).css( "color", "" );
-            var index = newLinks.indexOf(element);
-            if (index > -1) {
-                newLinks.splice(index, 1);
-            }
-            visitedLinks[url] = getType(element);
-            storage.set();
-            url = 'http://www.isnichwahr.de' + url;
-            if (isTampermonkey) {
-                GM_openInTab(url, {'active': !open_in_background});
-            } else {
-                GM_openInTab(url, open_in_background);
-            }
-        }
-
-        // remove upper pagination
-        $('.view-link-liste .text-center:first').remove();
-        // add buttons instead
-        controls = $('<ul class="pagination"></ul>');
-        $('<div class="text-center"><div/>').prependTo('.view-link-liste').append(controls);
-        var uuid = storageUri.split('/').pop();
-        var $uuidInput = $('<input type="text">').attr('size', uuid.length).val(uuid).change(function (event) {
-            var oldUri = storageUri;
-            storageUri='https://api.myjson.com/bins/' + $uuidInput.val();
-            storage.get().done( function(data) {
-                localStorage.setItem("visitedLinksUUID", storageUri);
-                localStorage.setItem("visitedLinks", JSON.stringify({}));
-                window.location.reload();
-            }).fail( function () {
-                alert("invalid id");
-                storageUri = oldUri;
-                uuid = storageUri.split('/').pop();
-                $uuidInput.val(uuid);
-            });
-        });
-        $('<li></li>').css("vertical-align","top").css("padding", "5px 10px")
-                .html($uuidInput).appendTo(controls);
-        // populate buttons
-        function addButton(text, callback) {
-            $('<li><a class="hans">' + text + '</a></li>').appendTo(controls).click(callback);
-        }
-        addButton('Mark all as seen!', function() {
-            $currentLinks.each( function(i, element) {
-                var url = $(this).attr('href');
-                $(this).css( "color", "" );
-                visitedLinks[url] = getType(this);
-
-            });
-            storage.set();
-            updateVisited();
-        });
-        addButton('Open all pics!', function() {
-            var i = newLinks.length;
-            var opened = 0;
-            while (i--) {
-                var link = newLinks[i];
-                if (opened < 5 && getType(link) === "pics") {
-                    opened++;
-                    openLink(link, true);
-                }
-            }
-        });
-        addButton('Open all videos!', function() {
-            var i = newLinks.length;
-            var opened = 0;
-            while (i--) {
-                var link = newLinks[i];
-                if (opened < 5 && (getType(link) === "video")) {
-                    opened++;
-                    openLink(link, true);
-                }
-            }
-        });
-        addButton('Open all new!', function() {
-            var i = newLinks.length;
-            var opened = 0;
-            while (i--) {
-                var link = newLinks[i];
-                if (opened < 5) {
-                    opened++;
-                    openLink(link, true);
-                }
-            }
-        });
-        $('a.hans').css({"cursor": "pointer"});
+function openInTab(url, open_in_background) {
+    if (isTampermonkey) {
+        GM_openInTab(url, {'active': !open_in_background})
+    } else {
+        GM_openInTab(url, open_in_background)
     }
 }
+
+async function insertApp() {
+    const links = document.querySelectorAll('.view-link-liste tbody tr td:nth-child(3) a')
+    let linkListRaw = []
+    links.forEach((element, i) => {
+        linkListRaw[i] = {
+            href: element.href,
+            type: getType(element),
+            visited: false,
+            text: element.text
+        }
+    })
+    const body = document.getElementsByTagName('body')[0];
+    body.outerHTML = `
+    <style>
+    ul {
+      width:760px;
+      overflow:hidden;
+    }
+    li {
+      line-height:1.5em;
+      float:left;
+      display:inline;
+    }
+    .double li  { width:50%;}
+    .buttons li {
+        float: left;
+        padding: 5px 10px;
+        background-color: #ffffff;
+        border: 1px solid #dddddd;
+        margin-left: -1px;
+    }
+    </style>
+      <body>
+      <div id='app'>
+         <ul class="buttons">
+          <li><a href="/list">1</a></li>
+          <li v-for="page in [1,2,3,4]">
+            <a v-bind:href="'/list?page='+page">{{page+1}}</a>
+          </li>
+        </ul>
+        <ul class="buttons">
+          <li style="vertical-align: top; padding: 5px 10px;">
+            <input v-model="uuid" type="text" size="0">
+          </li>
+          <li><a v-on:click='markAll'>Mark all as seen!</a></li>
+          <li><a v-on:click='openLinks("pic")'>Open all pics!</a></li>
+          <li><a v-on:click='openLinks("gif")'>Open all gifs!</a></li>
+          <li><a v-on:click='openLinks("video")'>Open all videos!</a></li>
+          <li><a v-on:click='openLinks("")'>Open all new!</a></li>
+        </ul>
+        <h2>other</h2>
+        <ul class="double">
+          <li v-for="link in other">
+            <a v-on:click.prevent="openLink(link)"
+               v-bind:style="{color: link.visited ? 'red' : ''}"
+               v-bind:href="link.href"
+            > {{ link.text }} </a>
+          </li>
+        </ul>
+        <h2>pics</h2>
+        <ul class="double">
+          <li v-for="link in pics">
+            <a v-on:click.prevent="openLink(link)"
+               v-bind:style="{color: link.visited ? 'red' : ''}"
+               v-bind:href="link.href"
+            > {{ link.text }} </a>
+          </li>
+        </ul>
+        <h2>videos</h2>
+        <ul class="double">
+          <li v-for="link in videos">
+            <a v-on:click.prevent="openLink(link)"
+               v-bind:style="{color: link.visited ? 'red' : ''}"
+               v-bind:href="link.href"
+            > {{ link.text }} </a>
+          </li>
+        </ul>
+        <h2>gifs</h2>
+        <ul class="double">
+          <li v-for="link in gifs">
+            <a v-on:click.prevent="openLink(link)"
+               v-bind:style="{color: link.visited ? 'red' : ''}"
+               v-bind:href="link.href"
+            > {{ link.text }} </a>
+          </li>
+        </ul>
+      </div>
+      </body>
+    `
+
+    const storage = new Storage()
+
+    const appIns = new Vue({
+      el: '#app',
+      data: {
+        uuid: '',
+        linkList: linkListRaw,
+      },
+      watch: {
+        linkList: {
+            handler: (old, newVal) => {
+                let visited = new Set(storage.get())
+                let visitedThis = newVal.filter( x => x.visited).map(x => x.href)
+                storage.set([...new Set([...visited, ...visitedThis])]); // array union
+            },
+            deep: true
+        },
+        uuid: function() {
+            if (storage.storageUri != this.uuid) {
+                storage.init(this.uuid)
+            }
+        }
+      },
+      computed: {
+          pics: function () { return this.linkList.filter(x => x.type == "pic")},
+          gifs: function () { return this.linkList.filter(x => x.type == "gif")},
+          videos: function () { return this.linkList.filter(x => x.type == "video")},
+          other: function () { return this.linkList.filter(x => x.type === false)}
+      },
+      methods: {
+        markAll: function () {
+            this.linkList.forEach( x => x.visited = true)
+        },
+        linksOfType: function(type) {
+            return this.linkList.filter( x => x.type == type)
+        },
+        openLinks: function (type) {
+            let opened = 0
+            for (let link of this.linkList.filter( x => !x.visited)) {
+                if (opened == 5) {
+                  break
+                }
+                if (link.type === type || type === "") {
+                  link.visited = true
+                  openInTab(link.href, true)
+                  opened++
+                }
+            }
+        },
+        openLink: link => {openInTab(link.href, false); link.visited = true}
+      }
+    })
+
+    await storage.init()
+    let visitedLinks = new Set(storage.get())
+    for (let link of appIns.linkList) {
+        if (visitedLinks.has(link.href)) {
+            link.visited = true;
+        }
+    }
+
+    appIns.uuid = storage.storageUri
+}
+// matches link list
+if (window.location.href.indexOf('isnichwahr.de/list') > -1) {
+    insertApp()
+}
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, {presets: ['es2016', 'es2017']});
+eval(c.code);
+/* jshint ignore:end */
